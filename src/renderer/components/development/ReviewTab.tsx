23324795c94219c138962399ed93f2cf3b30ffd1
Kenny Matsudo <kennymatsudo@live.com>
2026-03-25 16:25:59 -0700

    Fix infinite render loop in diff view

--- src/renderer/components/development/ReviewTab.tsx
+++ src/renderer/components/development/ReviewTab.tsx
@@ -8,6 +8,7 @@ import { useApprovalQueueStore } from '../../stores/approvalQueueStore';
 import { toast } from '../../stores/toastStore';
 import { openExternalUrl } from '../../services/shellService';
 
+
 interface ReviewTabProps {
   session: DevSessionWithPlanItem;
 }
